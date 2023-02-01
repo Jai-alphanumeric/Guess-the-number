63edacb160c5863e58638a58e53bfc8cd3b26327
Jai-alphanumeric <91368981+Jai-alphanumeric@users.noreply.github.com>
2023-02-01 21:41:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ let highscore = 0;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
 document.querySelector(".again").addEventListener("click", function () {
-    score = 20;
-    secretNumber = Math.trunc(Math.random()*20+1);
-    console.log(secretNumber);
-    document.querySelector(".number").textContent = '?';
-    document.querySelector(".guess").value = '';
-    document.querySelector(".score").textContent = score;
-    document.querySelector(".message").textContent = "Start Guessing...";
-    document.querySelector(".number").style.animation = 'none' ;
-    document.querySelector("body").style.animation = 'none';
-    document.querySelector(".number").style.fontSize = '3rem' ;
-})
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  console.log(secretNumber);
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".message").textContent = "Start Guessing...";
+  document.querySelector(".number").style.animation = "none";
+  document.querySelector("body").style.animation = "none";
+  document.querySelector(".number").style.fontSize = "3rem";
+});
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -23,13 +23,12 @@ document.querySelector(".check").addEventListener("click", function () {
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "Correct Number";
     document.querySelector(".number").textContent = secretNumber;
-    document.querySelector(".number").style.animation = 'aurora 5s ease-in-out infinite' ;
-    document.querySelector(".number").style.fontSize = '6rem' ;
-    document.querySelector("body").style.animation =
-    "aurora-bg 5s ease-in-out 1";
-    if(score > highscore){
-        document.querySelector('.highscore').textContent = score;
-        highscore= score;
+    document.querySelector(".number").style.animation = "aurora 5s ease-in-out infinite";
+    document.querySelector(".number").style.fontSize = "4rem";
+    document.querySelector("body").style.animation = "aurora-bg 5s ease-in-out 1";
+    if (score > highscore) {
+      document.querySelector(".highscore").textContent = score;
+      highscore = score;
     }
   } else if (guess < secretNumber) {
     if (score > 1) {
